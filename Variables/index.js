cd5259bce54513887a2891a738c42901b4bdef70
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2025-08-01 04:54:51 -0700

    variable completed

--- Variables/index.js
+++ Variables/index.js
@@ -1,12 +1,17 @@
 const accountId =  1234567
 let accountEmail= "aman@gmail.com"
 var password  = "1235"
-
-
 accountCity = "jaipur" 
+let accountState;
+
 accountEmail = "an@gmail.com"
  password  = "33465"
 accountCity = "haripur"
 console.log(accountId,);
-console.table([accountId,accountEmail,password, accountCity])
+/*
+Prefer not to use var 
+because of issue in block scope
+ and function scope */
+
+console.table([accountId,accountEmail,password, accountCity,accountState])
 
