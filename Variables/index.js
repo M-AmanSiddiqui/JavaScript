c18cb8a279813b5860039bfd6cabf2a651922f08
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2025-08-01 04:29:00 -0700

    first commit

---
+++ Variables/index.js
@@ -0,0 +1,12 @@
+const accountId =  1234567
+let accountEmail= "aman@gmail.com"
+var password  = "1235"
+
+
+accountCity = "jaipur" 
+accountEmail = "an@gmail.com"
+ password  = "33465"
+accountCity = "haripur"
+console.log(accountId,);
+console.table([accountId,accountEmail,password, accountCity])
+
