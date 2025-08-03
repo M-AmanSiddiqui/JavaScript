357392b8bc166f46f86b304587f05a35599fc90b
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2025-08-03 04:53:55 -0700

    learning investigation

--- 02index.js
+++ 02index.js
@@ -20,8 +20,44 @@ let isLoggedIn = false
 
 
 // object
-console.log(typeof null); //object
+// console.log(typeof null); //object
 
-console.log(typeof undefined); //undefined
+// console.log(typeof undefined); //undefined
+
+
+let score = "33abc"
+
+console.log(typeof score); //string 
+console.log(typeof (score)); //string 
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber) //number 
+console.log(valueInNumber); // NaN 
+
+//agr hmary score varibale aesa hota  then 
+//let score = "33"
+//output 33
+
+//agr hmary score varibale aesa hota  then 
+//let score = "33abc"
+//output NaN
+
+//agr hmary score varibale aesa hota  then 
+//let score = null
+//output 0
+
+//agr hmary score varibale aesa hota  
+//let score = undefined
+//then output NAN
+
+//agr hmary score varibale aesa hota  
+//let score = true
+//then output 1
+
+//agr hmary score varibale aesa hota  then 
+//let score = false
+//output 0
+
 
 
