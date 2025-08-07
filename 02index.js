55312005474ea277c0314436f71795f7d8a1b48b
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2025-08-07 00:42:32 -0700

    start next

--- 02index.js
+++ 02index.js
@@ -72,3 +72,15 @@
 // ****************************************** Comparison *******************************************************//
 
 
+
+
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+console.log(undefined == 0); //false
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
+
+
+
