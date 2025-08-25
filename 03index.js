5ec6fb37af705776fb251bddf7d524c19f8e2cfc
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2025-08-25 02:43:56 -0700

    Learning js Object

--- 03index.js
+++ 03index.js
@@ -1,6 +1,11 @@
 
-    
-    
-//3/25/10 sec
-
-    
+//Objecct literals
+const User = {
+    name: "Aman",
+    Age:  18,
+    location: "Karachi",
+    isLoggedIn: false,
+    LastLoginDays: ["Mon", "Tue"]
+}
+console.log(User.Age);
+console.log(User["Age"])
