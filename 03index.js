f5b76a7b98b98039fc59da5d51bdd7783ac52834
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2025-08-25 03:40:57 -0700

    Learning json

--- 03index.js
+++ 03index.js
@@ -1,3 +1,52 @@
+//Objecct Singleton
+const tinderUser = {}
+
+tinderUser.id = "abc123"
+tinderUser.name = "Aman"
+
+// console.log(tinderUser);
+// console.log(tinderUser.hasOwnProperty('name')); //true
+// console.log(Object.keys(tinderUser)); //Array mei keys return krta hai
+// console.log(Object.values(tinderUser)); //Array mei value return krta hai
+
+const regularUser = {
+    email:"aman@gmailcom",
+    fullName: {
+        user: "Aman",
+        usertwo: {
+            userthree: "siddiqui"
+        }
+    }
+}
+//  console.log(regularUser.fullName.usertwo);
+
+
+const object1 = {1: "a", 2: "b"}
+
+
+const object2 = {3: "c", 4: "d"}
+// const obj3 = {object1,object2}
+// const obj3 = Object.assign({},object1,object2)
+const obj3 = {...object1,...object2}
+//console.log(obj3 );
+
+
+
+const course = {
+    courseName: "JS",
+    price: "100",
+    Teacher: "Aman"
+
+}
+
+const {Teacher} = course
+console.log(Teacher);
+
+
+
+
+
+
 
 //Objecct literals
 const mySym = Symbol("key1")
@@ -9,7 +58,18 @@ const User = {
     isLoggedIn: false,
     LastLoginDays: ["Mon", "Tue"]
 }
-console.log(User.Age);
-console.log(User["Age"])
+// console.log(User.Age);
+// console.log(User["Age"])
 
-console.log(User[mySym])
+// console.log(User[mySym])
+User.greeting = function () {
+    console.log("Hello JS User");
+    
+}
+User.greetingtwo = function () {
+    console.log(`Hello JS User, ${this.name}`);
+    
+}
+// console.log(User.greeting());
+// console.log(User.greetingtwo());
+
