9e91af320cee4d947901b6e63bae8a0ebfd30574
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2025-08-26 22:54:21 -0700

    learned break and continues

--- 06index.js
+++ 06index.js
@@ -1,8 +1,10 @@
 // Loop
 
-for (let i = 0; i <= 15; i++) {
-    const element = i
-    console.log(element);
-    
-    
-}
+for (let i = 1; i <= 15; i++) {
+    if(i ==5){
+        console.log('5 detect');
+        continue
+        
+    }
+    console.log(`value of i is ${i}`);
+} 
