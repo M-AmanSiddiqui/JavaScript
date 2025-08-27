98bb76261744a05a2e94aa2b76dddcd89e27fda6
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2025-08-26 20:02:07 -0700

    learning arror function

--- 04index.js
+++ 04index.js
@@ -1,25 +1,52 @@
-function add(num1 , num2) {
-    return  num1 + num2
+// function add(num1 , num2) {
+//     return  num1 + num2
     
-}
-console.log( add(333,"a"));
+// }
+// console.log( add(333,"a"));
 
 
 
 
-function UserLogin(username) {
-    if (username === undefined) {
-     console.log("Please enter a username");
-      return  
-    } 
-    return  `${username} 'Logged in`
+// function UserLogin(username) {
+//     if (username === undefined) {
+//      console.log("Please enter a username");
+//       return  
+//     } 
+//     return  `${username} 'Logged in`
     
+// }
+// console.log( UserLogin());
+
+
+// function one(){
+//     const username = "Aman"
+
+//     function two() {
+//         const web = "Youtbe"
+//         console.log(username);
+//         console.log(web);
+        
+//     }
+    
+//     two()
+// }
+// one()
+
+
+
+// normal function
+// function one(num) {
+//     return num + 1
+// }
+// console.log( one(3));
+
+
+//expression function
+const two = function (num) {
+    return num + 3
 }
-console.log( UserLogin());
+console.log(two(5));
 
-
-
-// scope
 
 
 
