5d2432cad1adb4046c5cea18e0b032bc8dfba6a1
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2025-08-26 21:22:05 -0700

    add true value and false value in js

--- 05index.js
+++ 05index.js
@@ -7,17 +7,41 @@
 //         break;
 // }
 
-const month = 3
-switch (month) {
-    case month > 12:
-        console.log("12");
-        break;
-    case month > 11:
-        console.log("11");
-        break;
+// const month = 3
+// switch (month) {
+//     case month > 12:
+//         console.log("12");
+//         break;
+//     case month > 11:
+//         console.log("11");
+//         break;
 
-    default:
-        console.log("Try again");
+//     default:
+//         console.log("Try again");
         
-        break;
-}
+//         break;
+// }
+
+
+
+// Falsy Values
+// false,
+// 0,
+// -0,
+// BigInt 0n,
+// "",
+// null,
+// undefined,
+// NaN
+
+
+// Truly Values
+// true,
+// "0",
+// 'false',
+// " ",
+// [],
+// {},
+// function(){},
+
+
