fb33f6f0b0d0a39a6b05e0582447863ae76aca1a
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2025-08-28 03:15:22 -0700

    learned reduce

--- 08index.js
+++ 08index.js
@@ -8,3 +8,21 @@
 // Without Scope
 // const mynums = num.map( (num) =>  num + 10)
 // console.log(mynums);
+
+
+
+// Reduce
+// with normal function
+// const num = [1,2,3,4,5]
+// const total = num.reduce(function (acc,curr) {
+//     console.log(`acc: ${acc} and curr: ${curr}`);
+    
+//     return acc + curr
+// },0 )
+// console.log(total);
+
+
+// with arrow function
+// const num = [1,2,3,4,5]
+// const total = num.reduce( (acc,curr) => acc+curr, 0)
+// console.log(total);
