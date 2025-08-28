8780f5bcc7783baa448d3f13d8468ad56ae30912
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2025-08-28 02:11:50 -0700

    learning all loops

--- 06index.js
+++ 06index.js
@@ -1,10 +1,67 @@
-// Loop
+// Loops
 
-for (let i = 1; i <= 15; i++) {
-    if(i ==5){
-        console.log('5 detect');
-        continue
+// for (let i = 1; i <= 15; i++) {
+//     if(i ==5){
+//         console.log('5 detect');
+//         continue
         
-    }
-    console.log(`value of i is ${i}`);
-} 
+//     }
+//     console.log(`value of i is ${i}`);
+// } 
+
+
+
+// While loop
+// let i = 1
+// while (i <= 10) {
+//     console.log(i);
+//     i = i + 1
+// }
+
+
+//do while loop
+// let a = 11
+// do {
+// console.log(a);
+// a++
+// }
+// while (a <= 10);
+
+
+// for of loop
+// let arr = "Hello world"
+// for (const i of arr) {
+//     console.log(i);
+// }
+
+
+
+// Maps
+// const map = new Map()
+// map.set('PK',"Pakistan")
+// console.log(map);
+
+
+
+
+// let obj = {
+//     name: "Aman",
+//     fname: "waqar",
+//     Course: "JS"
+
+// }
+
+
+// for in loop
+// const programing = ["jsx","ts","py"]
+// for (const key in programing) {
+//     console.log(programing[key]);
+    
+// }
+
+
+// 
+// for each loop
+//  const programing = ["jsx","ts","py"]
+//  programing.forEach( function (item) {} )
+
