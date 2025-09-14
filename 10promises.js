bd622ac0b2ab46921242a75f9a8da90e31598d16
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2025-09-14 04:05:00 -0700

    Learning promises

--- 10promises.js
+++ 10promises.js
@@ -1,5 +1,5 @@
 // const { use } = require("react");
-
+// Promise One
 // const promiseOne = new Promise(function(resolve, reject){
 //     // Do an async task
 //     // DB calls, cryptography, network
@@ -15,7 +15,7 @@
 
 
 
-
+// Promise Two
 // new Promise(function (resolve,reject) {
 //     setTimeout(function() {
 //         console.log("asyns task 2");
@@ -28,6 +28,7 @@
 
 
 
+//Promise Three
 // const promiseThree = new Promise(function(resolve, reject){
     
 // setTimeout(function(){
@@ -41,25 +42,64 @@
 // })
 
 
-const promiseFour = new Promise(function(resolve, reject){
+
+// Promise Four
+// const promiseFour = new Promise(function(resolve, reject){
    
-setTimeout(function(){
-    let error = false
-    if (!error){
- resolve({username: "AMAN", password: "SIDDIQUI@gmail.com"})
-    }else{
-        reject('ERROR: Something went wrong')
-    }
+// setTimeout(function(){
+//     let error = false
+//     if (!error){
+//  resolve({username: "AMAN", password: "SIDDIQUI@gmail.com"})
+//     }else{
+//         reject('ERROR: Something went wrong')
+//     }
    
-}, 1000)
-})
-promiseFour.then((user) => {
-console.log(user);
-return user.username
-})
-.then((username) => {
-    console.log(username);
-}).catch(function (error) {
-    console.log(error);
+// }, 1000)
+// })
+// promiseFour.then((user) => {
+// console.log(user);
+// return user.username
+// })
+// .then((username) => {
+//     console.log(username);
+// }).catch(function (error) {
+//     console.log(error);
     
-})
+// }).finally(() => {
+//     console.log("The promise is either resolved or rejected");
+// })
+
+
+
+
+
+
+
+
+
+// Promise Five
+// const promiseFive = new Promise(function(resolve, reject){
+   
+// setTimeout(function(){
+//     let error = true
+//     if (!error){
+//  resolve({username: "JS", password: "JS@gmail.com"})
+//     }else{
+//         reject('ERROR: JS went wrong')
+//     }
+   
+// }, 1000)
+// })
+
+// async function PromiseFive(){
+//    try {
+//      const response = await promiseFive
+//     console.log(response);
+//    } catch (error){
+//     console.log(error);
+//     }
+// }
+// PromiseFive()
+
+
+// 6:30
