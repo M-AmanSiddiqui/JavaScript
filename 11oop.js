0f00a1bd9fc708a3c4c945fd4080dd6c41740c4c
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2025-09-18 06:20:30 -0700

    oop completed

---
+++ 11oop.js
@@ -0,0 +1,7 @@
+function User(userName, isLoggedIn) {
+    this.userName = userName
+    this.isLoggedIn = isLoggedIn
+    return this
+}
+const userOne = new User("Aman",true)
+console.log(userOne);
